db61d44a740b7defcb4e115ad26f177647579979
Blockitifluy <blockitifluy@gmail.com>
2026-01-26 16:29:06 +0000

    added better interface functionality

--- src/eval.rs
+++ src/eval.rs
@@ -1,37 +1,70 @@
 //! Calculates a value
+use std::{error::Error, fmt};
+
 use crate::{expression::Expression, operator::Operator};
 
-fn get_operants_operator_of_expr(expr: &Expression, results: &[f32]) -> (f32, Operator, f32) {
+fn get_operants_operator_of_expr(
+    expr: &Expression,
+    results: &[f32],
+) -> Result<(f32, Operator, f32), EvalCalculationErr> {
     match expr {
         Expression::Whole {
             left,
             operator,
             right,
-        } => (*left, *operator, *right),
+        } => Ok((*left, *operator, *right)),
         Expression::Left {
             left,
             operator,
             right,
         } => {
-            let r_val = results[*right];
-            (*left, *operator, r_val)
+            let r_val_null = results.get(*right);
+            let Some(r_val) = r_val_null else {
+                return Err(EvalCalculationErr::UnorderedExpressions {
+                    index: *right,
+                    is_left: false,
+                    expr: *expr,
+                });
+            };
+            Ok((*left, *operator, *r_val))
         }
         Expression::Right {
             left,
             operator,
             right,
         } => {
-            let l_val = results[*left];
-            (l_val, *operator, *right)
+            let l_val_null = results.get(*left);
+            let Some(l_val) = l_val_null else {
+                return Err(EvalCalculationErr::UnorderedExpressions {
+                    index: *left,
+                    is_left: true,
+                    expr: *expr,
+                });
+            };
+            Ok((*l_val, *operator, *right))
         }
         Expression::Op {
             left,
             operator,
             right,
         } => {
-            let l_val = results[*left];
-            let r_val = results[*right];
-            (l_val, *operator, r_val)
+            let l_val_null = results.get(*left);
+            let Some(l_val) = l_val_null else {
+                return Err(EvalCalculationErr::UnorderedExpressions {
+                    index: *right,
+                    is_left: false,
+                    expr: *expr,
+                });
+            };
+            let r_val_null = results.get(*right);
+            let Some(r_val) = r_val_null else {
+                return Err(EvalCalculationErr::UnorderedExpressions {
+                    index: *right,
+                    is_left: false,
+                    expr: *expr,
+                });
+            };
+            Ok((*l_val, *operator, *r_val))
         }
     }
 }
@@ -41,18 +74,50 @@ fn get_operants_operator_of_expr(expr: &Expression, results: &[f32]) -> (f32, Op
 /// - `exprs`: A slice of `Expression`s
 /// # Returns
 /// The calculated number
-pub fn eval_calculation(exprs: &[Expression]) -> f32 {
+pub fn eval_calculation(exprs: &[Expression]) -> Result<f32, EvalCalculationErr> {
     let mut results: Vec<f32> = Vec::with_capacity(16);
 
     for expr in exprs.iter() {
-        let (l_ant, operator, r_ant) = get_operants_operator_of_expr(expr, &results);
+        let (l_ant, operator, r_ant) = get_operants_operator_of_expr(expr, &results)?;
 
         let compute: f32 = operator.compute(l_ant, r_ant);
 
         results.push(compute);
     }
-
-    results.shrink_to_fit();
     let eval = results.last().unwrap_or(&0.0f32);
-    *eval
+    Ok(*eval)
 }
+
+/// Used for errors about evalulating a calculation.
+#[derive(Debug)]
+pub enum EvalCalculationErr {
+    /// Raised when an expression (of the type `Op`, `Left`, `Right`) can't get the value of the
+    /// linked expression, because it hasn't been evalulated yet.
+    ///
+    /// This is normally caused by the expressions not being sorted or binded correctly.
+    UnorderedExpressions {
+        /// The index of the linked expression
+        index: usize,
+        /// Is the operant on the left
+        is_left: bool,
+        /// The expression raising the error
+        expr: Expression,
+    },
+}
+impl fmt::Display for EvalCalculationErr {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::UnorderedExpressions {
+                index,
+                is_left,
+                expr,
+            } => write!(
+                f,
+                "expressions are not in a valid order ({expr} {} linked {index})",
+                if *is_left { "left" } else { "right" }
+            ),
+        }
+    }
+}
+
+impl Error for EvalCalculationErr {}
--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,6 @@ pub mod expression;
 pub mod operator;
 pub mod token;
 
-#[cfg(test)]
-mod tests {
-    mod token_test;
-}
-
 use std::{env, io};
 
 use crate::{
@@ -25,11 +20,6 @@ macro_rules! verbose {
     };
 }
 
-macro_rules! is_flag_set {
-    ($args:expr, $short:literal, $long:literal) => {{ $args.contains(&$short.to_string()) || $args.contains(&$long.to_string()) }};
-    ($args:expr, $long:literal) => {{ $args.contains(&$long.to_string()) }};
-}
-
 macro_rules! input {
     () => {{
         let mut buffer = String::new();
@@ -39,8 +29,8 @@ macro_rules! input {
     }};
 }
 
-fn parse_calculation(buffer: String, is_verbose: bool, is_eval: bool) -> Vec<Expression> {
-    let cal = reduce_calculation(&buffer);
+fn parse_calculation(buffer: &str, is_verbose: bool, is_eval: bool) -> Vec<Expression> {
+    let cal = reduce_calculation(buffer);
 
     // Parsing
 
@@ -61,19 +51,48 @@ fn parse_calculation(buffer: String, is_verbose: bool, is_eval: bool) -> Vec<Exp
     exprs
 }
 
-fn main() {
-    let buffer = input!();
-    let args: Vec<String> = env::args().collect();
-
-    let is_verbose = is_flag_set!(args, "-v", "--verbose");
-    let is_eval = is_flag_set!(args, "--eval");
-
+fn calculate_buffer(buffer: &str, is_verbose: bool, is_eval: bool) {
     let exprs = parse_calculation(buffer, is_verbose, is_eval);
-
     if is_eval {
         verbose!(is_verbose, "\n# Eval");
 
-        let eval_result = eval_calculation(&exprs);
+        let eval_result = eval_calculation(&exprs).unwrap();
         println!("{}", eval_result);
     }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let mut is_verbose: bool = false;
+    let mut is_eval = true;
+
+    let mut positional_args = Vec::new();
+    for (i, arg) in args.into_iter().enumerate() {
+        if i == 0 {
+            continue;
+        }
+
+        let arg_slice = (&arg) as &str;
+        match arg_slice {
+            "-v" | "--verbose" => is_verbose = true,
+            "--no-eval" => is_eval = false,
+            _ => positional_args.push(arg),
+        }
+    }
+
+    if positional_args.is_empty() {
+        let buffer = input!();
+
+        calculate_buffer(&buffer, is_verbose, is_eval);
+    } else {
+        for cal in positional_args {
+            calculate_buffer(&cal, is_verbose, is_eval);
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    mod token_test;
 }
